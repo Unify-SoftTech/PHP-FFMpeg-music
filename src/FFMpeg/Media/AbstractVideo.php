ad619bfb5433dc6280415217d6df8adba8310721
saurabhunify <67503639+saurabhunify@users.noreply.github.com>
2021-08-25 15:20:04 +0530

    Update AbstractVideo.php

--- src/FFMpeg/Media/AbstractVideo.php
+++ src/FFMpeg/Media/AbstractVideo.php
@@ -192,7 +192,7 @@ abstract class AbstractVideo extends Audio
         if ($format instanceof AudioInterface) {
             if (null !== $format->getAudioKiloBitrate()) {
                 $commands[] = '-b:a';
-               // $commands[] = $format->getAudioKiloBitrate() . 'k';
+                $commands[] = $format->getAudioKiloBitrate() . 'k';
             }
             if (null !== $format->getAudioChannels()) {
                 $commands[] = '-ac';
